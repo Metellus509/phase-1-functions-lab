8a99956d16fce7f47156f174ddf61c87b7036f35
Metellus <metellusjunior56@gmail.com>
2025-05-21 10:57:05 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  const hqLocation = 42;
+  return Math.abs(pickupLocation - hqLocation);
+}
+
+function distanceFromHqInFeet(value){
+    const blocks=distanceFromHqInBlocks(value)
+    return blocks*264
+}
+
+function distanceTravelledInFeet(start,end){
+    const blocks =Math.abs(start-end)
+    return blocks*264
+}
+
+function calculatesFarePrice(start,end){
+    const blocks=Math.abs(start-end)
+    const feet=blocks*264
+    if(feet <= 400){
+       const price=0
+       return price
+    }
+    else if(feet>=400 && feet<=2000){
+        const price=2.56
+        return price
+    }
+    else if(feet>2000 && feet<2500){
+        const price=25
+        return price
+    }
+    else{
+        return "cannot travel that far"
+    }
+}
